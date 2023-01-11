3d9c84c491d44873b41f2a84611ffd84d7149569
Lev <hotimpulse@hotmail.com>
2023-01-11 23:36:44 +0700

    sem3 hw

--- try_catch/Sem3_hw.java
+++ try_catch/Sem3_hw.java
@@ -8,27 +8,39 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class Sem3_hw {
-    public static void main(String[] args) {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            System.out.print("***\n" + "Enter data, separated by spaces. Get a text file back!***\n" +
-                        "In no specific order, type in a surname/name/middle name/\n" + 
-                        "date of birth (dd.mm.yyyy)/phone number(10 digits) and gender(f/m):\n");
-            String input = br.readLine();
 
+    static String surname = null;
+    static String name = null;
+    static String middleName = null;
+    static String dobString = null;
+    static String gender = null;
+    static String phoneNumber = null;
+    static String[] data;
+    public static void main(String[] args) {
+
+        try {
+            checkInput();
+            printInfo();
+            writeDataToFile(surname, name, middleName, dobString, gender, phoneNumber);
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            main(args);
+        }
+    }
+
+    public static void checkInput() {
+                    
+        System.out.print("***\n" + "Enter data, separated by spaces. Get a text file back!***\n" +
+                    "In no specific order, type in a surname/name/middle name/\n" + 
+                    "date of birth (dd.mm.yyyy)/phone number(10 digits) and gender(f/m):\n");
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = br.readLine();
             String[] data = input.split(" ");
             if (data.length != 6) {
                 throw new IllegalArgumentException("Incorrect number of data points entered. Please enter 6 data points separated by spaces.");
             }
-
-            String surname = null;
-            String name = null;
-            String middleName = null;
-            String dobString = null;
-            String gender = "f";
-            String phoneNumber = null;
-
             for (String s : data) {
                 if (s.matches("[a-zA-Z]\\w{2,29}")) {
                     if (surname == null) surname = s;
@@ -42,7 +54,7 @@ public class Sem3_hw {
                     phoneNumber = s;
                 }
             }
-
+    
             if (surname == null)
                 throw new IllegalArgumentException("Surname not found in the input, please enter again with proper format");
             if (name == null)
@@ -56,24 +68,23 @@ public class Sem3_hw {
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
             dateFormat.setLenient(false);
             dateFormat.parse(dobString);
-
-            System.out.println("Surname: " + surname);
-            System.out.println("Name: " + name);
-            System.out.println("Middle name: " + middleName);
-            System.out.println("Date of birth: " + dobString);
-            System.out.println("Gender: " + gender);
-            System.out.println("Phone number: " + phoneNumber);
-            writeDataToFile(surname, name, middleName, dobString, gender, phoneNumber);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-            main(args);
-        } catch (ParseException e) {
-            System.out.println("Invalid date of birth format. Date of birth should be in the format dd.MM.yyyy.");
-            main(args);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ParseException e) {
+            System.out.println("Invalid date of birth format. Date of birth should be in the format dd.MM.yyyy.");
+            checkInput();
         }
     }
+
+    public static void printInfo(){
+        System.out.println("Surname: " + surname);
+        System.out.println("Name: " + name);
+        System.out.println("Middle name: " + middleName);
+        System.out.println("Date of birth: " + dobString);
+        System.out.println("Gender: " + gender);
+        System.out.println("Phone number: " + phoneNumber);
+    }
+
     public static void writeDataToFile(String surname, String name, String middleName, String dobString, String gender, String phoneNumber) {
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
