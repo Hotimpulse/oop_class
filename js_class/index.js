adcaea712220dba1f04bee2f9278c098e6bdbc6b
Hotimpulse <jz2@rambler.ru>
2023-07-16 13:02:08 +0300

    adding js seminar1 hw

---
+++ js_class/index.js
@@ -0,0 +1,15 @@
+// task 1
+let tempC = 20;
+
+function transformCtoF(temp) {
+    return (9/5) * temp + 32;
+}
+
+console.log(`Temperature: ${tempC}C, ${transformCtoF(tempC)}F`);
+
+
+// task 2
+let myName = "Lev";
+let admin = myName;
+
+console.log(admin);
