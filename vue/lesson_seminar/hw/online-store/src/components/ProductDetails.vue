d39e2e561b34469d80c8d8f83ac1a64272d8e0eb
Hotimpulse <jz2@rambler.ru>
2023-10-27 15:17:42 +0300

    docs: adding seminar 10 hw

---
+++ vue/lesson_seminar/hw/online-store/src/components/ProductDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Product: {{ product.name }}</h2>
+    <p>Price: {{ formattedPrice }}</p>
+    <p>Availability: {{ isProductInStock }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      product: {
+        id: 1,
+        name: "T-Shirt",
+        price: 99.99,
+        available: true,
+      },
+    };
+  },
+  computed: {
+    formattedPrice() {
+      return `$` + this.product.price;
+    },
+    isProductInStock() {
+      return this.product.available ? "Available" : "Out of stock";
+    },
+  },
+};
+</script>
+
+<style></style>
